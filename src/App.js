72e493d22df20cadf7d418c147ac54e38007c63b
Jeralin <jeralinjeralin@gmail.com>
2024-11-14 12:00:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter className="App">
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
