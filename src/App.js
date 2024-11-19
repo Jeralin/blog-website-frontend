935c6b421d433428ace1ed9b781c9cab5b500056
ASHIKAA02 <ashikar137@gmail.com>
2024-11-19 10:40:31 +0530

    Admin Login

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AdminLogin from './Components/Admin/AdminLogin';
+import AdminDashboard from './Components/Admin/AdminDashboard';
 
 function App() {
   return (
     <BrowserRouter className="App">
       <Routes>
         <Route path='/admin_login' element={<AdminLogin/>} />
+        <Route path='/admin_dashboard' element={<AdminDashboard/>} />
       </Routes>
     </BrowserRouter>
   );
