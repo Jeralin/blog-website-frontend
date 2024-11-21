04d07d3fb22bafe4ae4987a9c0a7d0c956aba22b
Jeralin <jeralinjeralin@gmail.com>
2024-11-21 09:54:59 +0530

    login

--- src/Components/Admin/AdminLogin.jsx
+++ src/Components/Admin/AdminLogin.jsx
@@ -8,8 +8,8 @@ function AdminLogin() {
       {/* <!-- Main Content --> */}
       <div class="container-fluid  ">
         <div class="row main-content bg-success text-center  ">
-          <div class="col-md-4 text-center company__info">
-            <img src={blog_logo1} className="logo" />
+          <div class="col-md-4 text-center company__info ">
+            <img src={blog_logo1} className="logo cursor-pointer " />
           </div>
           <div class="col-md-8 col-xs-12 col-sm-12 login_form ">
             <div class="container-fluid">
