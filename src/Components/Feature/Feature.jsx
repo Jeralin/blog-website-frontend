27628707dab77184ca20ab8457954a31dc1e78bc
Jeralin <jeralinjeralin@gmail.com>
2024-11-26 10:04:23 +0530

    userLandingPage

---
+++ src/Components/Feature/Feature.jsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useRef } from "react";
+import "./Feature.css";
+import t1 from "../../Assets/t1.jpg";
+import t2 from "../../Assets/t2.jpg";
+import t3 from "../../Assets/t3.jpg";
+import t4 from "../../Assets/t4.jpg";
+import t5 from "../../Assets/t5.jpg";
+import t6 from "../../Assets/t6.jpg";
+import t7 from "../../Assets/t7.jpg";
+import t8 from "../../Assets/t8.jpg";
+
+const Feature = () => {
+  return (
+    <div className="feature container">
+      <div className="card">
+        <img src={t1} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>12 mins read &bull; Published:Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t2} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t3} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t4} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t5} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t6} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t7} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t8} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t1} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t2} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t3} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t4} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t5} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t6} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={t7} alt="" />
+        <h2>Blog Title Generator</h2>
+        <h3>By Spidey</h3>
+        <p>10 mins read &bull; Published:2 days ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
